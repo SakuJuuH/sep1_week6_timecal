a126a071238c23d719a56c705fbe8eadda5ea0b1
SakuJuuH <heinonensaku@gmail.com>
2025-02-27 15:31:01 +0200

    feat: enhance TimeCal to accept user input for distance and speed

--- src/main/java/TimeCal.java
+++ src/main/java/TimeCal.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class TimeCal {
 	public static void main(String[] args) {
-		System.out.println(timeCalculator(10, 2));
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the Distance (x): ");
+		double x = scanner.nextDouble();
+		System.out.println("Enter the Speed (y): ");
+		double y = scanner.nextDouble();
+		double time = timeCalculator(x, y);
+		System.out.printf("Time taken to travel %.1f km at %.1f km/h: %.1f hours", x, y, time);
+
 	}
-	
+
 	public static double timeCalculator(double x, double y) {
 		if (y == 0) {
 			throw new IllegalArgumentException("Division by zero is not allowed.");
